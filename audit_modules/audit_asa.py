f8b4a24bbec943f36411fda46cdf3c40f2a44287
UncleSocks <ilisan.tyrone@gmail.com>
2024-05-09 22:55:29 +0800

    Update: Added new ASA CIS benchmark checks.

--- audit_modules/audit_asa.py
+++ audit_modules/audit_asa.py
@@ -10,4 +10,9 @@ def run_cis_cisco_asa_assessment(connection):
     #1.1.3 Ensure 'Master Key Passphrase' is Set
     general_parsers.compliance_check_with_expected_output(connection, "show running-config | include no.service.password-recovery", "1.1.4 Ensure 'Password Recovery' is disabled", 1, global_report_output)
     password_parsers.compliance_check_password_policy(connection, "show running-config password-policy", "1.1.5 Ensure 'Password Policy' is enabled", 1, global_report_output)
+    
+    general_parsers.compliance_check_with_expected_output(connection, "show running-config domain-name", "1.2.1 Ensure 'Domain Name' is set", 1, global_report_output)
+    general_parsers.compliance_check_with_expected_output(connection, "show running-config hostname", "1.2.2 Ensure 'Host Name' is set", 1, global_report_output)
+    
+    
     return global_report_output
