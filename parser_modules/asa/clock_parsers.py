55e5d467a68b7ba28edba86e88a64bd7d83f7dcb
UncleSocks <ilisan.tyrone@gmail.com>
2024-05-21 09:59:10 +0800

    Update: Added Cisco ASA NTP server parser.

--- parser_modules/asa/clock_parsers.py
+++ parser_modules/asa/clock_parsers.py
@@ -23,3 +23,25 @@ def compliance_check_ntp_authentication_key(connection, command, cis_check, leve
     current_configuration = ntp_authentication_key_list if ntp_authentication_key_list else None
     compliant = ntp_authentication_key_match is not None
     global_report_output.append(generate_report(cis_check, level, compliant, current_configuration))
+
+
+def compliance_check_ntp_server(connection, command, cis_check, level, global_report_output):
+    command_output = ssh_send(connection, command)
+
+    ntp_server_list = []
+
+    regex_pattern = re.compile(r'^ntp\s+server\s+(?P<address>(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})\s+key\s+(?P<key>\d+)\s+source\s+(?P<interface>\S+)(?=\n|$)', re.MULTILINE)
+    ntp_server_match = regex_pattern.findall(command_output)
+
+    if ntp_server_match:
+        for ntp_server in ntp_server_match:
+            address = ntp_server[0]
+            key = ntp_server[1]
+            interface = ntp_server[2]
+
+            current_ntp_server_info = {'Address':address, 'Key':key, 'Interface':interface}
+            ntp_server_list.append(current_ntp_server_info)
+
+    current_configuration = ntp_server_list if ntp_server_list else None
+    compliant = ntp_server_match is not None
+    global_report_output.append(generate_report(cis_check, level, compliant, current_configuration))
