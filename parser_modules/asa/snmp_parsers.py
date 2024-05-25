fdd09aa273cbe8b5ddb66c7aac8d8e4c7f4efe97
UncleSocks <ilisan.tyrone@gmail.com>
2024-05-26 01:20:18 +0800

    Update: Modified SNMP user regular expression to account for Auth and NoAuth v3.

--- parser_modules/asa/snmp_parsers.py
+++ parser_modules/asa/snmp_parsers.py
@@ -16,11 +16,11 @@ def compliance_check_disabled_snmp(global_report_output):
                        {'CIS Check':"1.11.2 Ensure 'snmp-server user' is set to 'v3 auth SHA'", 'Level':1},
                        {'CIS Check':"1.11.3 Ensure 'snmp-server host' is set to 'version 3'", 'Level':1},
                        {'CIS Check':"1.11.4 Ensure 'SNMP traps' is enabled", 'Level':1},
-                       {'CIS Check':"1.11.5 Ensure 'SNMP community string' is not the default string"}]
+                       {'CIS Check':"1.11.5 Ensure 'SNMP community string' is not the default string", 'Level':1}]
 
     for snmp_cis_check in snmp_cis_checks:
         compliant = "Not Applicable"
-        current_configuration = "SNMP not enabled or SNMP configuration is incomplete"
+        current_configuration = "SNMP not enabled or incomplete configuration"
         cis_check = snmp_cis_check['CIS Check']
         level = snmp_cis_check['Level']
         global_report_output.append(generate_report(cis_check, level, compliant, current_configuration))
@@ -57,17 +57,17 @@ def compliance_check_snmp_server_user(connection, command, cis_check, level, glo
     snmp_server_user_list = []
     non_compliant_snmp_server_user_counter = 0
 
-    regex_pattern = re.compile(r'^snmp-server\s+user\s+(?P<user>\S+)\s+(?P<group>\S+)\s+v3\s+engineID\s+\S+\s+encrypted\s+auth\s+(?P<auth_type>\S+)\s+\S+\s+priv\s+(?P<priv_type>(?:aes\s+\d+)|3des)\s+\S+', re.MULTILINE)
+    regex_pattern = re.compile(r'^snmp-server\s+user\s+(?P<username>\S+)\s+(?P<groupname>\S+)\s+v3\s+engineID\s+\S+\s+(?:encrypted\s+auth\s+(?:(?P<auth_type>\w+))\s+\S+\s+(?=\s*))?(?:priv\s+(?P<priv_type>(?:aes\s+\d+|3des))\s+\S+\s*(?=\s*))?$', re.MULTILINE)
     snmp_server_user_match = regex_pattern.findall(command_output)
 
     if snmp_server_user_match:
         for snmp_server_user in snmp_server_user_match:
             user_name = snmp_server_user[0]
             group_name = snmp_server_user[1]
-            auth_type = snmp_server_user[2]
-            priv_type = snmp_server_user[3]
+            auth_type = snmp_server_user[2] if snmp_server_user[2] else None
+            priv_type = snmp_server_user[3] if snmp_server_user[3] else None
 
-            if priv_type == "3des":
+            if priv_type == "3des" or priv_type == None or auth_type == None:
                 non_compliant_snmp_server_user_counter += 1
 
             current_snmp_server_user_info = {'User Name':user_name, 'Group Name':group_name, 'Auth Type':auth_type, 'Priv Type':priv_type}
